727be870e48c2eac0eb6b5423ef21bce548267b1
Gabriel Solomon <hello@gabrielsolomon.ro>
2019-10-29 16:39:55 +0200

    add StoppableEventInterface

---
+++ src/Events/StoppableEventInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace ByTIC\EventDispatcher\Events;
+
+/**
+ * Interface StoppableEventInterface
+ * @package ByTIC\EventDispatcher\Events
+ */
+interface StoppableEventInterface extends EventInterface, \Psr\EventDispatcher\StoppableEventInterface
+{
+}
